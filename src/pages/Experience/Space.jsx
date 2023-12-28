e692b68f5066ebdcfe212614879c0b9263e1ff31
paramthakkar123 <paramthakkar864@gmail.com>
2023-12-28 21:32:52 +0530

    Animations added

---
+++ src/pages/Experience/Space.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Space = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default Space;
---
+++ src/pages/Front/FrontPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Front from "./Front";
+import FrontCenter from "./FrontCenter";
+import FrontContent from "./FrontContent";
+
+const FrontPage = () => {
+  return (
+    <div>
+      <Front />
+      <FrontCenter />
+      <FrontContent />
+    </div>
+  );
+};
+
+export default FrontPage;
