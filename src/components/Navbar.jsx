c9243c27e65d93b5cac0d0ec4082ec95ea47afe2
paramthakkar123 <paramthakkar864@gmail.com>
2023-12-31 09:17:24 +0530

    Frontend Code added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+import gsap from "gsap";
+
+const Navbar = () => {
+  useEffect(() => {
+    gsap.from(".logo", {
+      x: -30,
+      opacity: 0,
+      delay: 1,
+    });
+    gsap.to(".logo", {
+      x: 0,
+      opacity: 1,
+      duration: 2,
+      ease: "power3.inOut",
+    });
+
+    gsap.from(".item", {
+      y: -300,
+      opacity: 0,
+      delay: 1,
+    });
+
+    gsap.to(".item", {
+      y: 0,
+      opacity: 1,
+      duration: 2,
+      ease: "expo.inOut",
+      stagger: {
+        each: 0.1,
+      },
+    });
+  }, []);
+  return (
+    <div className="sticky bg-black text-white z-10 top-0">
+      <div className="text-white pt-4 pl-6 pb-4 text-2xl flex items-center justify-between">
+        <div className="logo ml-10">Logo</div>
+        <div>
+          <div className="flex items-center uppercase text-2xl">
+            <Link
+              to="/"
+              className="item mr-8 hover:bg-pink-500 hover:text-white transition-all duration-300 rounded p-2"
+            >
+              Home
+            </Link>
+            <Link
+              to="/login"
+              className="item mr-8 hover:bg-pink-500 hover:text-white transition-all duration-300 rounded p-2"
+            >
+              Log In
+            </Link>
+            <Link
+              to="/signup"
+              className="item mr-8 hover:bg-pink-500 hover:text-white transition-all duration-300 rounded p-2"
+            >
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Experience/Images.js
@@ -0,0 +1,13 @@
+import ImageOne from "../../assets/space.jpg";
+import ImageTwo from "../../assets/nature.jpg";
+import ImageThree from "../../assets/city.jpg";
+import ImageFour from "../../assets/underwater.jpg";
+
+const Images = {
+  ImageOne,
+  ImageTwo,
+  ImageThree,
+  ImageFour,
+};
+
+export default Images;
