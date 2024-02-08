43b05122d7a4498ad984f83c5c1a0dbdc1491a33
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2024-02-08 23:42:46 +0530

    Update user.route.js

--- lofiapi/routes/user.route.js
+++ lofiapi/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {deleteUser, test, updateUser} from '../controllers/user.controller.js';
+import {deleteUser, feedback, test, updateUser} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/', test);
 router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
+router.post('/feedback', feedback)
 
 export default router;
