62f9b5628dbc659d349d4c775b78ea01d9e6bccc
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2024-02-08 23:41:42 +0530

    Update auth.controller.js

--- lofiapi/controllers/auth.controller.js
+++ lofiapi/controllers/auth.controller.js
@@ -1,70 +1,58 @@
-import User from '../models/user.model.js';
-import bcrypt from 'bcryptjs'
-import { errHandler } from '../utils/error.js';
-import jwt from 'jsonwebtoken';
+import Feedback from "../models/feedback.model.js";
+import User from "../models/user.model.js";
+import { errHandler } from "../utils/error.js";
 
-export const signup = async (req, res, next) => {
-    const { username, email, password, age, musicpreferences } = req.body;
-    const hashedPassword = await bcrypt.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword, age, musicpreferences });
-    try{
-        await newUser.save()
-        res.status(201).json({ message: 'User created' });
-    }catch(err){
-        next(err);
+export const test = (req, res) => {
+    res.send('Hello World');
+}
+
+export const feedback = async (req, res) => {
+  try{
+    const {email, response} = req.body;
+    const feedback = new Feedback({email, response});
+    await feedback.save();
+    res.status(201).json({message: 'Feedback sent'});
+  }catch(error){
+    console.log(error);
+  }
+}
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errHandler(401, 'You can update only your account!'));
+  }
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const login = async (req, res, next) => {
-    const {email, password} = req.body;
-    try{
-        const validUser = await User.findOne({ email });
-        if(!validUser) return next(errHandler(404, 'User not found'));
-        const validPassword = await bcrypt.compareSync(password, validUser.password);
-        if(!validPassword) return next(errHandler(401, 'Wrong Credentials'));
-        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
-        const {password: hashedPassword, ...rest} = validUser._doc;
-        res.cookie('access_token', token, {httpOnly: true}, {
-            maxAge: 24 * 60 * 60 * 1000,
-        }).status(200).json(rest);
-    }catch(error){
-        next(error);
-    }
-}
-
-export const google = async (req, res, next) => {
-    try{
-        const user = await User.findOne({email: req.body.email});
-
-        if(user){
-            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
-            const {password: hashedPassword, ...rest} = user._doc;
-            res.cookie('access_token', token, {httpOnly: true}, {
-                maxAge: 24 * 60 * 60 * 1000,
-            }).status(200).json(rest);
-        }
-        else{
-            const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
-            const hashedPassword = await bcrypt.hashSync(generatedPassword, 10);
-            const newUser = new User({username:
-          req.body.name.split(' ').join('').toLowerCase() +
-          Math.random().toString(36).slice(-8),
-        email: req.body.email,
-        password: hashedPassword,
-        profilePicture: req.body.photo,});
-            await newUser.save();
-            const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET);
-            const {password: hashedPassword2, ...rest} = newUser._doc;
-            res.cookie('access_token', token, {httpOnly: true}, {
-                maxAge: 24 * 60 * 60 * 1000,
-            }).status(200).json(rest);
-
-        }
-    }catch(error){
-        next(error);
-    }
-}
-
-export const signout = async (req, res, next) => {
-    res.clearCookie('access_token').status(200).json({message: 'Signout successfully'});
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errHandler(401, 'You can delete only your account!'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json('User has been deleted...');
+  } catch (error) {
+    next(error);
+  }
 }
