1ca6d901b6c2faae2b39c21382823c8d32df1915
ParamThakkar123 <paramthakkar864@gmail.com>
2024-09-10 17:14:40 +0530

    Removed unnecessary code

--- Frontend/.vite/deps/react-dom_client.js
+++
@@ -1,8 +0,0 @@
-import {
-  require_client
-} from "./chunk-WRTK2BGJ.js";
-import "./chunk-PQZINITG.js";
-import "./chunk-HAZNF34R.js";
-import "./chunk-WXXH56N5.js";
-export default require_client();
-//# sourceMappingURL=react-dom_client.js.map
--- Frontend/.vite/deps/react.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react
-} from "./chunk-HAZNF34R.js";
-import "./chunk-WXXH56N5.js";
-export default require_react();
-//# sourceMappingURL=react.js.map
