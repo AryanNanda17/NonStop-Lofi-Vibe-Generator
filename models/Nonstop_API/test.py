f1593efe4edabd462e87de2336347d62f9bdda5e
ParamThakkar123 <paramthakkar864@gmail.com>
2024-09-26 11:00:45 +0530

    A few changes made

--- models/Nonstop_API/test.py
+++
@@ -1,8 +0,0 @@
-from music21 import midi, stream
-s = stream.Score()
-mf = midi.MidiFile()
-mf.open('test_output.mid')
-mf.read()
-mf.close()
-s.append(midi.translate.midiFileToStream(mf))
-s.show('midi')
