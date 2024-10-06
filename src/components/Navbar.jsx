8180f43cfc3de9f474e154a181ffb6ee29e27760
ivaibhavkulkarni <vaibhav.kulkarni0359@gmail.com>
2024-10-06 14:21:37 +0530

    Dynamic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import "./navbar.css";
-import gsap from "gsap";
+import "./navbar.css"; // Ensure this file contains the updated styles
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
@@ -12,19 +11,15 @@ const Navbar = () => {
     let location = useLocation();
 
     useEffect(() => {
-        // Check if the window width is larger than a certain breakpoint (e.g., 1024px)
         const handleResize = () => {
             setIsDesktop(window.innerWidth > 1024);
         };
 
-        // Set initial value
         handleResize();
 
-        // Listen to window resize events
         window.addEventListener("resize", handleResize);
 
         return () => {
-            // Remove the event listener on component unmount
             window.removeEventListener("resize", handleResize);
         };
     }, []);
@@ -66,67 +61,119 @@ const Navbar = () => {
                 <div className="lg:hidden">
                     <button
                         type="button"
-                        className="text-white hover:text-gray-300 focus:outline-none focus:text-gray-300"
+                        className="text-white hover:text-gray-300 focus:outline-none focus:text-gray-300 mr-3"
                         onClick={toggleMenu}
                     >
                         ☰
                     </button>
                 </div>
-                <div>
-                    <div className="hidden lg:flex items-center uppercase text-2xl">
+
+                {/* Desktop Menu */}
+                <div className="hidden lg:flex items-center uppercase text-2xl">
+                    <Link
+                        to="/"
+                        className="item mr-8 bg-gradient-to-r from-violet-600 to-pink-500 transition-all duration-300 rounded-lg p-2"
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        to="/about"
+                        className="item mr-8 bg-gradient-to-r from-violet-600 to-pink-500 transition-all duration-300 rounded-lg p-2"
+                    >
+                        About
+                    </Link>
+                    {currentUser ? (
+                        <div className="flex items-center uppercase text-2xl mr-5">
+                            <Link to="/profile">
+                                <img
+                                    src={currentUser.profilePicture}
+                                    alt="profile"
+                                    className="item h-10 w-10 mr-4 rounded-full object-cover"
+                                />
+                            </Link>
+                            <Link to="/compose">Compose Music</Link>
+                        </div>
+                    ) : (
+                        <>
+                            <Link
+                                to="/contact"
+                                className="item mr-8 bg-gradient-to-r from-violet-600 to-pink-500 transition-all duration-300 rounded-lg p-2"
+                            >
+                                Contact
+                            </Link>
+                            <Link
+                                to="/login"
+                                className="item mr-8 bg-gradient-to-r from-violet-600 to-pink-500 transition-all duration-300 rounded-lg p-2"
+                            >
+                                Log In
+                            </Link>
+                            <Link
+                                to="/signup"
+                                className="item mr-8 bg-gradient-to-r from-violet-600 to-pink-500 transition-colors duration-300 rounded-lg p-2"
+                            >
+                                Sign up
+                            </Link>
+                        </>
+                    )}
+                </div>
+
+                {/* Mobile Menu */}
+                {isMenuOpen && !isDesktop && (
+                    <div className="mobile-menu absolute top-20 left-0 right-0 text-white flex flex-col items-end p-4 space-y-4">
                         <Link
                             to="/"
-                            className="item mr-8 bg-gradient-to-r from-violet-600 to-pink-500 transition-all duration-300 rounded-lg p-2"
+                            className="item block bg-gradient-to-r from-violet-600 to-pink-500 transition-all duration-300 rounded-lg p-2"
+                            onClick={toggleMenu} // Close menu after click
                         >
                             Home
                         </Link>
                         <Link
                             to="/about"
-                            className="item mr-8 bg-gradient-to-r from-violet-600 to-pink-500 transition-all duration-300 rounded-lg p-2"
+                            className="item block bg-gradient-to-r from-violet-600 to-pink-500 transition-all duration-300 rounded-lg p-2"
+                            onClick={toggleMenu} // Close menu after click
                         >
                             About
                         </Link>
                         {currentUser ? (
-                            <div className="hidden lg:flex items-center uppercase text-2xl mr-5">
-                                <Link to="/profile">
+                            <div className="flex flex-col items-center">
+                                <Link to="/profile" onClick={toggleMenu}>
                                     <img
                                         src={currentUser.profilePicture}
                                         alt="profile"
-                                        className="item h-10 w-10 mr-4 rounded-full object-cover"
+                                        className="item h-10 w-10 rounded-full object-cover mb-2"
                                     />
                                 </Link>
-                                <Link to="/compose">Compose Music</Link>
+                                <Link to="/compose" onClick={toggleMenu}>
+                                    Compose Music
+                                </Link>
                             </div>
                         ) : (
-                            <div className="hidden lg:flex items-center uppercase text-2xl">
-                                {/* <Link
-                  to="/research"
-                  className="item mr-8 bg-gradient-to-r from-violet-600 to-pink-500 transition-all duration-300 rounded-lg p-2"
-                >
-                  Research
-                </Link> */}
+                            <>
                                 <Link
                                     to="/contact"
-                                    className="item mr-8 bg-gradient-to-r from-violet-600 to-pink-500 transition-all duration-300 rounded-lg p-2"
+                                    className="item block bg-gradient-to-r from-violet-600 to-pink-500 transition-all duration-300 rounded-lg p-2"
+                                    onClick={toggleMenu} // Close menu after click
                                 >
                                     Contact
                                 </Link>
                                 <Link
                                     to="/login"
-                                    className="item mr-8 bg-gradient-to-r from-violet-600 to-pink-500 transition-all duration-300 rounded-lg p-2"
+                                    className="item block bg-gradient-to-r from-violet-600 to-pink-500 transition-all duration-300 rounded-lg p-2"
+                                    onClick={toggleMenu} // Close menu after click
                                 >
                                     Log In
                                 </Link>
                                 <Link
                                     to="/signup"
-                                    className="item mr-8 bg-gradient-to-r from-violet-600 to-pink-500 transition-colors duration-300 rounded-lg p-2"
+                                    className="item block bg-gradient-to-r from-violet-600 to-pink-500 transition-colors duration-300 rounded-lg p-2"
+                                    onClick={toggleMenu} // Close menu after click
                                 >
                                     Sign up
                                 </Link>
-                            </div>
+                            </>
                         )}
                     </div>
-                </div>
+                )}
             </div>
         </div>
     );
