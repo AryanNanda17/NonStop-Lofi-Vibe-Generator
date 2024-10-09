615fcbe42c296691c3ebd40528939bfef41c9ce7
Chetan Dayapule <12220018+ChetanDayapule@users.noreply.github.com>
2024-10-09 11:30:26 +0000

    Footer improvements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,36 +2,37 @@ import React from "react";
 import './footer.css';
 
 const Footer = () => {
+  const GIT_URL = "https://github.com/AryanNanda17/NonStop-Lofi-Vibe-Generator";
   return (
     <div className="bg-black">
       <footer className="footer">
         <div className="button-container">
           <a title="About Us" href="/about">About Us</a>
-          <span class="px-2">|</span>
+          <span className="delimeter">|</span>
           <a title="Feedback" href="/contact">Feedback</a>
-          <span class="px-2">|</span>
-          <a title="Terms of Use" href="/about">Terms of Use</a>
-          <span class="px-2">|</span>
-          <a title="Privacy Policy" href="/about">Privacy Policy</a>
+          <span className="delimeter">|</span>
+          <a title="Terms of Use" target="_blank" href={GIT_URL}>Terms of Use</a>
+          <span className="delimeter">|</span>
+          <a title="Privacy Policy" target="_blank" href={GIT_URL}>Privacy Policy</a>
         </div>
         <div className="social-links">
-          <a href="/" target="_blank" rel="noopener noreferrer" className="link">
+          <a href={GIT_URL} target="_blank" rel="noopener noreferrer" className="link">
             <img src="./icons/icons8-facebook.svg" alt="Facebook" />
           </a>
-          <a href="/" target="_blank" rel="noopener noreferrer" className="link">
+          <a href={GIT_URL} target="_blank" rel="noopener noreferrer" className="link">
             <img src="./icons/icons8-twitter.svg" alt="Twitter" />
           </a>
-          <a href="/" target="_blank" rel="noopener noreferrer" className="link">
+          <a href={GIT_URL} target="_blank" rel="noopener noreferrer" className="link">
             <img src="./icons/icons8-instagram.svg" alt="Instagram" />
           </a>
-          <a href="/" target="_blank" rel="noopener noreferrer" className="link">
+          <a href={GIT_URL} target="_blank" rel="noopener noreferrer" className="link">
             <img src="./icons/icons8-youtube.svg" alt="Youtube" />
           </a>
         </div>
       </footer>
-      <copyrights className="copyrights">
+      <div className="copyrights">
         Experience the harmony of tomorrow, today, with AI-composed tunes.
-      </copyrights>
+      </div>
     </div>
   );
 };
